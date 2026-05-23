378dad8b1fb8a13385975a21ea9c6ba09895d0cc
dat.dn <dat.dn@skymap.com>
2026-05-23 11:35:32 +0700

    Add parameter and TDD

--- hello.go
+++ hello.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
-func Hello() string {
-	return "Hello World"
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
+
 }
